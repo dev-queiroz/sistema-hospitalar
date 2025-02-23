bbb061be6f56d2a1d24f5a190409c3bc1e9bd8d6
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2025-02-22 22:52:32 -0300

    Vou recomeçar devagar.

--- criptografar_seha.js
+++
@@ -1,9 +0,0 @@
-// criptografar a senha "!@#$%¨&*(" e mostrar a senha criptografada
-const bcrypt = require('bcryptjs');
-
-const saltRounds = 10;
-const password = "!@#$%¨&*(";
-
-bcrypt.hash(password, saltRounds, function (err, hash) {
-    console.log(hash);
-});
