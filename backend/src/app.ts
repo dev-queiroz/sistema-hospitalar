447a37aecb663970ae65ae9b69c91cb72ffaa25e
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2025-02-23 11:26:39 -0300

    Commit

--- backend/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import unidadeRouter from './packages/unidade/unidade.routes';
-
-const app = express();
-
-app.use(express.json());
-app.use('/unidades', unidadeRouter);
-
-export default app;
